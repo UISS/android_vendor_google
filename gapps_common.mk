c8004e9d0a49f209268e8817387fe8081a6cb2c3
OldDroid <alex.pohl1998@live.de>
2013-02-26 23:40:30 +0100

    initial tiny gapps

---
+++ gapps_common.mk
@@ -0,0 +1,19 @@
+# Google Apps
+PRODUCT_COPY_FILES += \
+	vendor/google/armv6/app/GoogleBackupTransport.apk:system/app/GoogleBackupTransport.apk \
+	vendor/google/armv6/app/GoogleCalendarSyncAdapter.apk:system/app/GoogleCalendarSyncAdapter.apk \
+	vendor/google/armv6/app/GoogleContactsSyncAdapter.apk:system/app/GoogleContactsSyncAdapter.apk \
+	vendor/google/armv6/app/GoogleLoginService.apk:system/app/GoogleLoginService.apk \
+	vendor/google/armv6/app/GoogleServicesFramework.apk:system/app/GoogleServicesFramework.apk \
+	vendor/google/armv6/app/PlayStore.apk:system/app/PlayStore.apk \
+	vendor/google/armv6/app/SetupWizard.apk:system/app/SetupWizard.apk \
+	vendor/google/armv6/app/NetworkLocation.apk:system/app/NetworkLocation.apk \
+	vendor/google/armv6/app/Talk.apk:system/app/Talk.apk \
+	vendor/google/armv6/app/ChromeBookmarksSyncAdapter.apk:system/app/ChromeBookmarksSyncAdapter.apk
+
+PRODUCT_COPY_FILES += \
+        vendor/google/armv6/etc/permissions/com.google.android.maps.xml:system/etc/permissions/com.google.android.maps.xml \
+        vendor/google/armv6/etc/permissions/com.google.android.media.effects.xml:system/etc/permissions/com.google.android.media.effects.xml
+
+PRODUCT_COPY_FILES += \
+	vendor/google/armv6/framework/com.google.android.maps.jar:system/framework/com.google.android.maps.jar
