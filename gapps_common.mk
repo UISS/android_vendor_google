6a7572b3ca2fe4c8c408d2d98ce0eec17b6c5c18
singh_dd93 <singh_dd93@yahoo.in>
2013-02-27 12:46:28 +0530

    Fixed the paths
    Change-Id: I3c2e9aa492579615297d95e059806e4253e3a50f

--- gapps_common.mk
+++ gapps_common.mk
@@ -1,19 +1,19 @@
 # Google Apps
 PRODUCT_COPY_FILES += \
-	vendor/google/armv6/app/GoogleBackupTransport.apk:system/app/GoogleBackupTransport.apk \
-	vendor/google/armv6/app/GoogleCalendarSyncAdapter.apk:system/app/GoogleCalendarSyncAdapter.apk \
-	vendor/google/armv6/app/GoogleContactsSyncAdapter.apk:system/app/GoogleContactsSyncAdapter.apk \
-	vendor/google/armv6/app/GoogleLoginService.apk:system/app/GoogleLoginService.apk \
-	vendor/google/armv6/app/GoogleServicesFramework.apk:system/app/GoogleServicesFramework.apk \
-	vendor/google/armv6/app/PlayStore.apk:system/app/PlayStore.apk \
-	vendor/google/armv6/app/SetupWizard.apk:system/app/SetupWizard.apk \
-	vendor/google/armv6/app/NetworkLocation.apk:system/app/NetworkLocation.apk \
-	vendor/google/armv6/app/Talk.apk:system/app/Talk.apk \
-	vendor/google/armv6/app/ChromeBookmarksSyncAdapter.apk:system/app/ChromeBookmarksSyncAdapter.apk
+	vendor/google/common/app/GoogleBackupTransport.apk:system/app/GoogleBackupTransport.apk \
+	vendor/google/common/app/GoogleCalendarSyncAdapter.apk:system/app/GoogleCalendarSyncAdapter.apk \
+	vendor/google/common/app/GoogleContactsSyncAdapter.apk:system/app/GoogleContactsSyncAdapter.apk \
+	vendor/google/common/app/GoogleLoginService.apk:system/app/GoogleLoginService.apk \
+	vendor/google/common/app/GoogleServicesFramework.apk:system/app/GoogleServicesFramework.apk \
+	vendor/google/common/app/PlayStore.apk:system/app/PlayStore.apk \
+	vendor/google/common/app/SetupWizard.apk:system/app/SetupWizard.apk \
+	vendor/google/common/app/NetworkLocation.apk:system/app/NetworkLocation.apk \
+	vendor/google/common/app/Talk.apk:system/app/Talk.apk \
+	vendor/google/common/app/ChromeBookmarksSyncAdapter.apk:system/app/ChromeBookmarksSyncAdapter.apk
 
 PRODUCT_COPY_FILES += \
-        vendor/google/armv6/etc/permissions/com.google.android.maps.xml:system/etc/permissions/com.google.android.maps.xml \
-        vendor/google/armv6/etc/permissions/com.google.android.media.effects.xml:system/etc/permissions/com.google.android.media.effects.xml
+        vendor/google/common/etc/permissions/com.google.android.maps.xml:system/etc/permissions/com.google.android.maps.xml \
+        vendor/google/common/etc/permissions/com.google.android.media.effects.xml:system/etc/permissions/com.google.android.media.effects.xml
 
 PRODUCT_COPY_FILES += \
-	vendor/google/armv6/framework/com.google.android.maps.jar:system/framework/com.google.android.maps.jar
+	vendor/google/common/framework/com.google.android.maps.jar:system/framework/com.google.android.maps.jar
